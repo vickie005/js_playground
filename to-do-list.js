9393d623f976995ab5f9c758820acef8e904fd3b
vickie005 <mwendwavictory29@gmail.com>
2025-05-29 12:58:27 +0300

    loops and arrays

---
+++ to-do-list.js
@@ -0,0 +1,53 @@
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '22-12-2025'
+}, {
+        name: 'wash dishes',
+        dueDate: '22-12-2025'
+    }];
+
+renderTodoList();
+
+function renderTodoList() {
+    let todoListHTML = '';
+ 
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const { name, dueDate } = todoObject;
+        const html = `
+            <div> ${name} </div>
+           <div>${dueDate}</div>
+            <button onclick="
+            todoList.splice(${i}, 1);
+            renderTodoList();
+            " class="delete-todo-button">Delete</button>
+        `;
+        todoListHTML += html;
+    }
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
+}
+
+function addToDo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+
+    todoList.push({
+      // name: name,
+      // dueDate: dueDate
+      name, 
+      dueDate, // }<= shorthand property
+    });
+
+    inputElement.value = '';
+
+    renderTodoList();
+}
+    
